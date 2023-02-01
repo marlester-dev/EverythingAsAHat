32aeb2bf5d5a518e41ec18c7fc325c2a1d03814d
Marlester <mserik1234go@gmail.com>
2023-02-01 15:56:23 +0600

    Backported to 1.16.5

--- src/main/java/net/marlester/everythingasahat/mixin/ESMixin.java
+++ src/main/java/net/marlester/everythingasahat/mixin/ESMixin.java
@@ -3,14 +3,14 @@ package net.marlester.everythingasahat.mixin;
 import net.minecraft.block.AbstractSkullBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class) // Previously (1.16 or 1.17 - 1.14) it was MobEntity
+@Mixin(MobEntity.class) // In 1.18 or 1.17+ it is LivingEntity
 public class ESMixin {
     @Inject(at = @At("HEAD"), method = "getPreferredEquipmentSlot", cancellable = true)
     private static void getPreferredEquipmentSlot(ItemStack itemStack, CallbackInfoReturnable<EquipmentSlot> cir) {
