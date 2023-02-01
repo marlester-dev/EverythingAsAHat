ecc3c3f60d750532a19353770e4036d48edcfa81
Marlester <mserik1234go@gmail.com>
2023-02-01 14:30:02 +0600

    Fix Typos

--- src/main/java/net/marlester/everythingasahat/mixin/ESMixin.java
+++ src/main/java/net/marlester/everythingasahat/mixin/ESMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class) /// Previously (1.16 or 1.17 - 1.14) it was MobEntity
+@Mixin(LivingEntity.class) // Previously (1.16 or 1.17 - 1.14) it was MobEntity
 public class ESMixin {
     @Inject(at = @At("HEAD"), method = "getPreferredEquipmentSlot", cancellable = true)
     private static void getPreferredEquipmentSlot(ItemStack itemStack, CallbackInfoReturnable<EquipmentSlot> cir) {
