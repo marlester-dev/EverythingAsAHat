02c8bc0dc3933710e378fdebbb924c59e94e7d83
Marlester <mserik1234go@gmail.com>
2023-02-06 10:17:52 +0600

    Transfer all recent changes & bump version to 0.0.2

--- src/main/java/net/marlester/everythingasahat/mixin/ESMixin.java
+++ src/main/java/net/marlester/everythingasahat/mixin/ESMixin.java
@@ -1,27 +1,24 @@
 package net.marlester.everythingasahat.mixin;
 
 import net.minecraft.block.Blocks;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MobEntity.class) // In 1.18 or 1.17+ it is LivingEntity
+@Mixin(targets = {"net/minecraft/screen/PlayerScreenHandler$1"})
 public class ESMixin {
-    @Inject(at = @At("HEAD"), method = "getPreferredEquipmentSlot", cancellable = true)
-    private static void getPreferredEquipmentSlot(ItemStack itemStack, CallbackInfoReturnable<EquipmentSlot> cir) {
+    EquipmentSlot field_7834;
+    @Inject(method = "canInsert(Lnet/minecraft/item/ItemStack;)Z", at = @At("RETURN"), cancellable = true)
+    public void allowItemEquipping(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
         Item item = itemStack.getItem();
-        // !(item instanceof SkullItem) does not properly work, so I have to list every skull/head
-        if (!(item instanceof ElytraItem) && !(item instanceof ShieldItem) && !(item instanceof ArmorItem) && item != Blocks.CARVED_PUMPKIN.asItem() && item != Items.SKELETON_SKULL && item != Items.WITHER_SKELETON_SKULL && item != Items.CREEPER_HEAD && item != Items.DRAGON_HEAD && item != Items.PLAYER_HEAD && item != Items.ZOMBIE_HEAD) {
-            cir.setReturnValue(EquipmentSlot.HEAD);
-        }
-        // fix for shift+lmb items
-        if (Screen.hasShiftDown() && !(item instanceof ElytraItem) && !(item instanceof ShieldItem) && !(item instanceof ArmorItem) && item != Blocks.CARVED_PUMPKIN.asItem() && item != Items.SKELETON_SKULL && item != Items.WITHER_SKELETON_SKULL && item != Items.CREEPER_HEAD && item != Items.DRAGON_HEAD && item != Items.PLAYER_HEAD && item != Items.ZOMBIE_HEAD) {
-            cir.setReturnValue(EquipmentSlot.MAINHAND);
+        if (field_7834 == EquipmentSlot.HEAD && item != Items.ELYTRA && item != Items.SHIELD && !(item instanceof ArmorItem) && item != Blocks.CARVED_PUMPKIN.asItem() && item != Items.SKELETON_SKULL && item != Items.WITHER_SKELETON_SKULL && item != Items.CREEPER_HEAD && item != Items.DRAGON_HEAD && item != Items.PLAYER_HEAD && item != Items.ZOMBIE_HEAD) {
+            cir.setReturnValue(true);
         }
     }
 }
