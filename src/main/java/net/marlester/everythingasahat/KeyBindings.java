54838cb52bd8bcd9c8d76dc721e3a868a51310eb
Marlester <77095872+marlester-dev@users.noreply.github.com>
2023-06-04 18:25:45 +0200

    commit clean

---
+++ src/main/java/net/marlester/everythingasahat/KeyBindings.java
@@ -0,0 +1,44 @@
+package net.marlester.everythingasahat;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBindings implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        //to-do: if player tries to wear elytra, armor, shield, etc. send the right message and also make the config option for it
+        //to-do 2: spread it on all (mc) versions of the mod
+        KeyBinding keyBinding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding("everythingasahat.keybinding_1", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H, "everythingasahat.key.category"));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding1.wasPressed()) {
+                PlayerEntity player = MinecraftClient.getInstance().player;
+                if (!player.getMainHandStack().isEmpty()) {
+                    MinecraftClient CLIENT = MinecraftClient.getInstance();
+
+                    final int source = player.getInventory().selectedSlot;
+                    final int destination = 5; // helmet slot, I can find all of them on imgur.com/4jLf3l5
+
+                    CLIENT.interactionManager.clickSlot (
+                            player.playerScreenHandler.syncId,
+                            destination,
+                            source,
+                            SlotActionType.SWAP,
+                            player
+                    );
+                } else {
+                    player.sendMessage(Text.translatable("everythingasahat.mainhand.is.empty.while.pressing.keybinding1"), false);
+                    break;
+                }
+                player.sendMessage(Text.translatable("everythingasahat.keybinding1.equipped.item.on.head.successfully"), false);
+            }
+        });
+    }
+}
